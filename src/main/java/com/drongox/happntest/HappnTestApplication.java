a5cd8fbbfce7973bf07babfa377fe32d2c7132d7
ilya.zinchuk <ilya.zinchuk@renault.com>
2019-07-15 20:15:36 +0200

    project structure created

---
+++ src/main/java/com/drongox/happntest/HappnTestApplication.java
@@ -0,0 +1,13 @@
+package com.drongox.happntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappnTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappnTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drongox/happntest/HappnTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.drongox.happntest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HappnTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
